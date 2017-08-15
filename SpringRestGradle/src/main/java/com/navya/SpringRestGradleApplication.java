b5e0ea72f3bc4a7f9e91f54b75bd982e9c0df135
navyareddy06 <navyareddykoppula@gmail.com>
2017-08-15 15:45:19 -0500

    Add files via upload

---
+++ SpringRestGradle/src/main/java/com/navya/SpringRestGradleApplication.java
@@ -0,0 +1,12 @@
+package com.navya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestGradleApplication.class, args);
+	}
+}
